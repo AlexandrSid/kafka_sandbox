d395d0a278cb76808cde41a3c6992f6c293706c3
Aleksandr <alexandr.sidorkin@gmail.com>
2025-06-26 13:37:19 +0200

    Stage3 complete. Topic config is in separate component

--- kafka-config/build.gradle.kts
+++ kafka-config/build.gradle.kts
@@ -1,30 +1,32 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.5.3"
-	id("io.spring.dependency-management") version "1.1.7"
+    java
+    id("org.springframework.boot") version "3.5.3"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.aleksid"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.kafka:spring-kafka")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.kafka:spring-kafka-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.kafka:spring-kafka")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- kafka-config/src/main/java/com/aleksid/kafka_config/KafkaConfigApplication.java
+++ kafka-config/src/main/java/com/aleksid/kafka_config/KafkaConfigApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KafkaConfigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaConfigApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConfigApplication.class, args);
+    }
 
 }
