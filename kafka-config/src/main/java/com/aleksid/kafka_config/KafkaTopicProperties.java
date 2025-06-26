108386395f4b8e83c70b95dcff82d4a9179f2d81
Aleksandr <alexandr.sidorkin@gmail.com>
2025-06-26 17:25:59 +0200

    Stage4 works Cluster config works with kafka ui

--- kafka-config/src/main/java/com/aleksid/kafka_config/KafkaTopicProperties.java
+++ kafka-config/src/main/java/com/aleksid/kafka_config/KafkaTopicProperties.java
@@ -1,16 +1,24 @@
 package com.aleksid.kafka_config;
 
 import lombok.Data;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Configuration
 @ConfigurationProperties(prefix = "spring.kafka")
 @Data
 public class KafkaTopicProperties {
     private List<TopicConfig> topics;
+//    @Value("${spring.kafka.bootstrap-servers}")
+//    private String bootstrapServers;
 
     @Data
     public static class TopicConfig {
@@ -18,4 +26,11 @@ public class KafkaTopicProperties {
         int partitions;
         int replicas;
     }
+//
+//    @Bean
+//    public KafkaAdmin kafkaAdmin() {
+//        Map<String, Object> config = new HashMap<>();
+//        config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+//        return new KafkaAdmin(config);
+//    }
 }
